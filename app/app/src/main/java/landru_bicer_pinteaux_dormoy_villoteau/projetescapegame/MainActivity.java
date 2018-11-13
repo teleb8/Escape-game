956dd320247de9d177d72a4aacc215a91ac800bb
pierre villoteau <pierre.villoteau@hotmail.fr>
2018-11-13 13:44:02 +0100

    mise en place

---
+++ app/app/src/main/java/landru_bicer_pinteaux_dormoy_villoteau/projetescapegame/MainActivity.java
@@ -0,0 +1,13 @@
+package landru_bicer_pinteaux_dormoy_villoteau.projetescapegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
